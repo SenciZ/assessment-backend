24684e72706fa924702a9d12fadbff171951f6a6
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-03 23:05:33 -0700

    add comments

--- client/main.js
+++ client/main.js
@@ -3,68 +3,58 @@ const fortuneButton = document.getElementById('fortuneButton')
 const fortuneDisplay = document.getElementById('fortuneDisplay')
 const addNewPicture = document.getElementById('addImage')
 const pictureContainer = document.getElementById('pictureContainer')
-
 const newFortuneForm= document.getElementById('newFortune')
+const newImageForm = document.getElementById('newImage')
+
+
 newFortuneForm.addEventListener('submit', newFortuneHandler);
 
-
-
-const newImageForm = document.getElementById('newImage')
 newImageForm.addEventListener('submit', submitHandler);
 
-const errCallback = err => console.log(err)
 
 const getNewCompliment = () => axios.get('http://localhost:4004/api/compliment').then(complimentCallback).catch(errCallback);
-const complimentCallback = (response) => {const data = response.data; alert(data);}
-
 
 const getNewFortune = () => axios.get('http://localhost:4004/api/fortune').then(fortuneCallback).catch(errCallback);
-const fortuneCallback = (response) => {fortuneDisplay.textContent = "Your Fortune is... "+ response.data.fortune;}
-
-
 
 const getAllImages = () => axios.get('http://localhost:4004/api/pictures').then(imageCallback).catch(errCallback);
+
 const addNewImage = body => axios.post('http://localhost:4004/api/pictures', body).then(imageCallback).catch(errCallback)
 
 const addNewFortune = body => axios.post('http://localhost:4004/api/fortune', body).then(alert("Your new fortune of " + body.fortune + " has been added!")).catch(errCallback)
 
 const deleteImage = id => axios.delete(`http://localhost:4004/api/pictures/${id}`).then(imageCallback).catch(errCallback)
 
+const complimentCallback = (response) => {const data = response.data; alert(data);}
 const imageCallback = ({data: images}) => renderImages(images);
+const fortuneCallback = (response) => {fortuneDisplay.textContent = "Your Fortune is... "+ response.data.fortune;}
+const errCallback = err => console.log(err)
 
 
 function submitHandler(e){
     e.preventDefault();
     let imageURL = document.getElementById('img')
-
+    
     let imgObj = {
         imageURL: imageURL.value,
     }
-
+    
     addNewImage(imgObj)
     imageURL.value = '';
 }
 
+//Creates a new fortune when new fortune form is submitted
 function newFortuneHandler(e){
     e.preventDefault();
     let newFortune = document.getElementById('fortuneId')
-
+    //Creates new fortune object
     let fortuneObj = {
         fortune: newFortune.value,
     }
-
+//Calls addNewFortune post request with the new object as argument
     addNewFortune(fortuneObj)
+//Resets value of input to empty
     newFortune.value = '';
 }
-
-
-function createImageCard(image){
-    const imageCard = document.createElement('div')
-    imageCard.classList.add('imageCard')
-    imageCard.innerHTML = `<img class="happyImage" alt="Happy Image" src="${image.imageURL}"><button class="deleteBtn" onclick="deleteImage(${image.id})">Delete Image</button>`
-    pictureContainer.appendChild(imageCard)
-}
-
 
 function renderImages(arr){
     pictureContainer.innerHTML = '';
@@ -72,18 +62,24 @@ function renderImages(arr){
         createImageCard(arr[i])
     }
 }
+
+function createImageCard(image){
+    const imageCard = document.createElement('div')
+    imageCard.classList.add('imageCard')
+    const happyImage = document.createElement('img')
+    happyImage.setAttribute('alt', "Happy Image")
+    happyImage.setAttribute('src', image.imageURL)
+    const deleteImage = document.createElement("button")
+    deleteImage.textContent = "Delete Image"
+    deleteImage.setAttribute("onclick", `deleteImage(${image.id})`)
+    imageCard.appendChild(happyImage)
+    imageCard.appendChild(deleteImage)
+    pictureContainer.appendChild(imageCard)
+}
+
+
 getNewFortune()
 getAllImages();
-
-
-
-
-
-
-
-
-
-
 
 
 
--- server/controller.js
+++ server/controller.js
@@ -1,54 +1,54 @@
-const fortunes = require('./fortunedb.json')
-const pictures = require('./imagedb.json')
-let imageId = 4
+const fortunes = require("./fortunedb.json");
+const pictures = require("./imagedb.json");
+let imageId = 4;
 
-module.exports= {
-    getImages: (req, res) =>{
-        res.status(200).send(pictures)
-    },
+module.exports = {
+  getImages: (req, res) => {
+    res.status(200).send(pictures);
+  },
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!",
-                           "Cool shirt!",
-                           "Your Javascript skills are stellar.",
-        ];
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-    
-        res.status(200).send(randomCompliment);     
-      },
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
 
-      getFortune: (req, res) => {
-          let randomFortune = Math.floor(Math.random()* fortunes.length);
-          res.status(200).send(fortunes[randomFortune])
-      },
-      addFortune: (req, res) =>{
-        const {fortune} = req.body
-        let newFortune = {
-            fortune
-        }
-        fortunes.push(newFortune)
-        console.log(fortunes)
-        res.status(200).send(newFortune)
-    },
+    res.status(200).send(randomCompliment);
+  },
 
-      
-      deletePicture: (req,res) =>{
-        const {id} = req.params;
-        let index = pictures.findIndex(image => +image.id === +id)
-        pictures.splice(index, 1)
-        res.status(200).send(pictures);
-    },
+  getFortune: (req, res) => {
+    let randomFortune = Math.floor(Math.random() * fortunes.length);
+    res.status(200).send(fortunes[randomFortune]);
+  },
+  addFortune: (req, res) => {
+    const { fortune } = req.body;
+    let newFortune = {
+      fortune,
+    };
+    fortunes.push(newFortune);
+    console.log(fortunes);
+    res.status(200).send(newFortune);
+  },
 
-    addPicture: (req, res) =>{
-        const {imageURL} = req.body
-        let newImage = {
-            imageURL,
-            id: imageId
-        }
-        imageId++
-        pictures.push(newImage)
-        res.status(200).send(pictures)
-    }
-}
+  deletePicture: (req, res) => {
+    const { id } = req.params;
+    let index = pictures.findIndex((image) => +image.id === +id);
+    pictures.splice(index, 1);
+    res.status(200).send(pictures);
+  },
+
+  addPicture: (req, res) => {
+    const { imageURL } = req.body;
+    let newImage = {
+      imageURL,
+      id: imageId,
+    };
+    imageId++;
+    pictures.push(newImage);
+    res.status(200).send(pictures);
+  },
+};
